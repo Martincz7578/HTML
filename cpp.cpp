ea49c12236e93e42b9f4ef93480ac3d80fe048ec
Martin <it2422@sspu-opava.cz>
2024-10-03 07:40:54 +0200

    Update cpp.cpp

--- cpp.cpp
+++ cpp.cpp
@@ -1,36 +1,5 @@
 #include <iostream>
-#include <cmath>
 
-double n1, n2;
-char op;
-
-using namespace std;
-
-cout << "enter 1st number";
-cin >> n1;
-if(cin.fail()){
-cout << "fail";
+int main(){
+  std::cout "hello world!\n";
 }
-
-cout << "enter operator";
-cin >> op;
-
-cout << "enter 2nd number";
-cin >> n2;
-if(cin.fail()){
-cout << "fail";
-}
-
-switch (op){
-case '+':
-cout << n1 + n2;
-break;
-case '-';
-cout << n1 - n2;
-break;
-case '*':
-cout << n1 * n2;
-break;
-case '/':
-cout << n1 / n2;
-break;
